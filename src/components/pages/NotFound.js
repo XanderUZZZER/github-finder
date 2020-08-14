7f59d000e58bd3a2229761924fd4c1804d06cba3
XanderUZZZER <xanderuzzzer@gmail.com>
2020-08-14 15:18:43 +0300

    notFound home link added

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
     <div>
       <h1>Not Found</h1>
       <p className='lead'>The page you are looking for does not exist</p>
+      <h2>
+        <Link to="/">
+          <i className="fas fa-home" /> Back home
+        </Link>
+      </h2>
     </div>
   )
 }
